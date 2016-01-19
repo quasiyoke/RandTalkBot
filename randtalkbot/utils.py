73cb7da5bdf7501266206bde1777a0fd091d5f8d
Pyotr Ermishkin <quasiyoke@gmail.com>
2016-01-19 08:19:36 +0300

    Bumped version number to 1.0.3.

---
+++ randtalkbot/utils.py
@@ -0,0 +1,11 @@
+# RandTalkBot Bot matching you with a random person on Telegram.
+# Copyright (C) 2016 quasiyoke
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from os import path
+
+__version__ = '1.0.3'
+RANDTALKBOT_DIR = path.abspath(path.dirname(__file__))
+LOCALE_DIR = path.join(RANDTALKBOT_DIR, 'locale')
