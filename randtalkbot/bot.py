2728467d518accc549fa2ff602b33b21acff2f8b
Pyotr Ermishkin <quasiyoke@gmail.com>
2016-03-08 06:10:48 +0300

    Bot._calculate_*_seed -> telepot.delegate.per_from_id_*

--- randtalkbot/bot.py
+++ randtalkbot/bot.py
@@ -9,7 +9,7 @@ import logging
 import telepot
 from .admin_handler import AdminHandler
 from .stranger_handler import StrangerHandler
-from telepot.delegate import per_chat_id
+from telepot.delegate import per_from_id_in, per_from_id_except
 from telepot.async.delegate import create_open
 
 LOGGER = logging.getLogger('randtalkbot.bot')
@@ -20,23 +20,15 @@ class Bot:
         self._delegator_bot = telepot.async.DelegatorBot(
             configuration.token,
             [
-                (self._calculate_admin_seed, create_open(AdminHandler, stranger_service)),
-                (self._calculate_stranger_seed, create_open(StrangerHandler, stranger_service)),
+                # If the bot isn't chatting with an admin, skip, so for this chat will be used another handler,
+                # not AdminHandler.
+                (per_from_id_in(self._admins_telegram_ids), create_open(AdminHandler, stranger_service)),
+                # If the bot is chatting with an admin, skip, so for this chat will be used another handler,
+                # not StrangerHandler.
+                (per_from_id_except(self._admins_telegram_ids), create_open(StrangerHandler, stranger_service)),
                 ]
             )
         self._stranger_service = stranger_service
-
-    def _calculate_admin_seed(self, message_json):
-        telegram_id = message_json['chat']['id']
-        # If the bot isn't chatting with an admin, skip, so for this chat will be used another handler,
-        # not AdminHandler.
-        return telegram_id if telegram_id in self._admins_telegram_ids else None
-
-    def _calculate_stranger_seed(self, message_json):
-        telegram_id = message_json['chat']['id']
-        # If the bot is chatting with an admin, skip, so for this chat will be used another handler,
-        # not StrangerHandler.
-        return None if telegram_id in self._admins_telegram_ids else telegram_id
 
     @asyncio.coroutine
     def run(self):
